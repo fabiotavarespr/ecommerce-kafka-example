71428c768138d29e12cd422085702ab7e6d9b412
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-12-28 20:24:56 -0300

    Create a report example

---
+++ service-users/src/main/java/dev/fabiotavarespr/ecommerce/BatchSendMessageService.java
@@ -0,0 +1,64 @@
+package dev.fabiotavarespr.ecommerce;
+
+import dev.fabiotavarespr.ecommerce.entity.User;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class BatchSendMessageService {
+
+    private final Connection connection;
+    private final KafkaDispatcher<User> userDispatcher = new KafkaDispatcher<>();
+
+    BatchSendMessageService() throws SQLException {
+        String url = "jdbc:sqlite:.data/db/users_database.db";
+        this.connection = DriverManager.getConnection(url);
+        try {
+            connection.createStatement().execute("create table Users(" +
+                    "uuid varchar(200) primary key," +
+                    "email varchar(200))");
+        } catch (SQLException ex) {
+            //Be Careful, the sql could be wrong, be reallly careful
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var batchService = new BatchSendMessageService();
+        try (var service = new KafkaService<>(
+                BatchSendMessageService.class.getSimpleName(),
+                "SEND_MESSAGE_TO_ALL_USERS",
+                batchService::parse,
+                String.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, String> record) throws SQLException, ExecutionException, InterruptedException {
+        System.out.println("----------------------------------------------");
+        System.out.println("Processing new batch");
+        System.out.println("Topic: " + record.value());
+        var order = record.value();
+
+        for (User user : getAllUsers()) {
+            userDispatcher.send(record.value(), user.getUuid(), user);
+
+        }
+    }
+
+    private List<User> getAllUsers() throws SQLException {
+        var results = connection.prepareStatement("select uuid from Users").executeQuery();
+        List<User> users = new ArrayList<>();
+        while (results.next()) {
+            users.add(new User(results.getString("uuid")));
+        }
+        return users;
+    }
+}
